6a6cf92682c6baaef62467b6d94858bf3483c266
chris berry and haley bristol <chrisberry@yahoo.com>
2016-03-16 16:59:38 -0700

    start of google maps project

---
+++ README.md
@@ -0,0 +1,10 @@
+npm init
+npm install gulp -g
+npm install bower -g
+bower init
+bower install jquery --save
+
+npm install --save-dev vinyl-source-stream gulp-sass del browserify jshint gulp-jshint gulp browser-sync gulp-uglify gulp-util gulp-concat gulp-sourcemaps bower-files
+
+gulp build   //build developer folders//
+gulp serve   //deploys developer folders to local server//
---
+++ js/browser-interface.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+  $('#locateUser').click(locateUser);
+});
+
+//google maps functions
+function locateUser() {
+  // If the browser supports the Geolocation API
+  if (navigator.geolocation){
+    var positionOptions = {
+      enableHighAccuracy: true,
+      timeout: 10 * 1000 // 10 seconds
+    };
+    navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+  }
+  else {
+    alert("Your browser doesn't support the Geolocation API");
+  }
+}
