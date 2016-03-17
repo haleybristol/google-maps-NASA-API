7181189f336fdb98e723b75f99593513db5c8447
chris berry and haley bristol <chrisberry321@yahoo.com>
2016-03-17 13:20:57 -0700

    add google API

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,18 +1,41 @@
-$( document ).ready(function() {
+var apiKey = require('../.env').apiKey;
+
+$(document).ready(function(){
   $('#locateUser').click(locateUser);
 });
 
-//google maps functions
 function locateUser() {
-  // If the browser supports the Geolocation API
+
   if (navigator.geolocation){
     var positionOptions = {
       enableHighAccuracy: true,
-      timeout: 10 * 1000 // 10 seconds
+      timeout: 10 * 1000
     };
     navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
   }
   else {
     alert("Your browser doesn't support the Geolocation API");
   }
+}
+
+function geolocationSuccess(position) {
+console.log(position)
+  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+  var myOptions = {
+    zoom : 16,
+    center : userLatLng,
+    mapTypeId : google.maps.MapTypeId.ROADMAP
+  };
+
+  var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+
+  new google.maps.Marker({
+    map: mapObject,
+    position: userLatLng
+  });
+}
+
+function geolocationError(positionError) {
+  alert(positionError);
 }
