122bb9b7234afef5d0807037bab1207387199a46
chris berry and haley bristol <hebristol@gmail.com>
2016-03-17 15:17:41 -0700

    add mars photos

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,4 +1,5 @@
 var apiKey = require('../.env').apiKey;
+var nasaKey = require('../.env').nasaKey;
 
 $(document).ready(function(){
   $('#locateUser').click(locateUser);
@@ -19,7 +20,6 @@ function locateUser() {
 }
 
 function geolocationSuccess(position) {
-console.log(position)
   var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
   var myOptions = {
@@ -27,7 +27,7 @@ console.log(position)
     center : userLatLng,
     mapTypeId : google.maps.MapTypeId.ROADMAP
   };
-
+  console.log(myOptions);
   var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
 
   new google.maps.Marker({
@@ -39,3 +39,18 @@ console.log(position)
 function geolocationError(positionError) {
   alert(positionError);
 }
+
+$(document).ready(function(){
+  $('#curiosity').submit(function(event){
+    event.preventDefault();
+    var date = $('#date').val();
+    var fcam =  $('#camera').val('fcam');
+    var rcam =  $('#camera').val('rcam');
+    var mcam =  $('#camera').val('mcam');
+    $.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=' + date + '&api_key=C7uaHPQUmVeGK9TU4bTOkuIMDxo8d4zoJWS8utaa').then(function(response){
+      console.log(response);
+      var mars = response.photos[0].img_src
+      $('.picResult').append('<img src=' + mars + '>' );
+    });
+  });
+});
